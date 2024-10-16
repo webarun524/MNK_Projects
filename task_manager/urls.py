d757f7f9a9c6d3f0567abb70a8d885f7bb2d5657
Arun Kumar M <arunofficial669@gmail.com>
2024-10-16 18:01:13 +0530

    Add files via upload

---
+++ task_manager/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.task_list, name='task_list'),
+    path('add/', views.add_task, name='add_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
